55b32373c98713bdd75a67aa62fbf78d2c024ed1
Usman Saleem <usman.saleem@codeupscale.com>
2025-01-14 10:37:47 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World React JS
+         Hello Usman React JS
         </p>
         <a
           className="App-link"
